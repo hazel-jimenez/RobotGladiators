f006a24caaf637879bb6111f35b62fe2c218967b
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-01-11 18:16:46 -0500

    added player and enemy

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,21 @@
-window.alert ("This is an alert! JavaScript is running!");
+//player name, health, & attack var
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//enemy name, health, & attack var
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//fight function
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+//execute fight
+
+fight();
